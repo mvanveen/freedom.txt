997d4da21e1c8bd025508b0eb25ca5de908c7da2
Michael Van Veen <michael@mvanveen.net>
2012-01-23 22:14:33 -0800

    modified script to keep adresses in yaml

--- freedom.py
+++ freedom.py
@@ -1,12 +1,19 @@
 import re
+import socket
 import sys
+from urlparse import urlparse
+
+import yaml
 
 def main():
   with open('freedom_template.txt', 'r') as file_obj:
     inp = file_obj.read()
 
   with open('locations.txt', 'r') as file_obj:
-    locations = set(file_obj.readlines())
+    user_locations = set(file_obj.readlines())
+
+  with open('poi.txt', 'r') as file_obj:
+    poi_locations = set(file_obj.readlines())
 
   name = raw_input('What is your name? ')
   if not name:
@@ -15,12 +22,28 @@ def main():
 
   inp = re.sub('{name}', name, inp)
 
+  get_addrs = lambda locations: dict([(
+    location,
+    socket.gethostbyname(
+      urlparse(location).netloc)
+    ) for location in locations
+  ])
+
+  set_addrs = lambda locations: dict([(
+    x.replace('\n', ''), y) for x, y in
+     sorted(locations.iteritems(), key=lambda x: x[0])
+  ])
+
   inp = re.sub(
     '{locations}',
-    '\n'.join(x.replace('\n', '') for x in sorted(locations)),
+    '---\n' + yaml.dump({
+      'humans': set_addrs(get_addrs(user_locations)),
+      'poi': set_addrs(get_addrs(poi_locations))
+    }, default_flow_style=False )+ '---\n',
     inp
   )
 
+  print inp
   location = raw_input('Where should I write file to [freedom.txt]? ')
 
   print
